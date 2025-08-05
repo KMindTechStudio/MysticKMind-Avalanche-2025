24dbc5cbe1117dc0cc3b7daed105bb520ad26be4
MinTech <ngonguyenkminh.dsn@gmail.com>
2025-08-05 17:34:58 +0700

    NPC Taliking Script

--- Assets/Scripts/NPC Scripts/NPC_Talk.cs
+++ Assets/Scripts/NPC Scripts/NPC_Talk.cs
@@ -5,6 +5,7 @@ public class NPC_Talk : MonoBehaviour
     private Rigidbody2D rb;
     private Animator anim;
     public Animator interactAnim;
+    public DialogueSO dialogueSO;
 
     private void Awake()
     {
@@ -24,5 +25,16 @@ public class NPC_Talk : MonoBehaviour
     {
         interactAnim.Play("Close");
         rb.bodyType = RigidbodyType2D.Dynamic;
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Interact"))
+        {
+            if (DialogueManager.Instance.isDialogueActive)
+                DialogueManager.Instance.AdvanceDialogue();
+            else
+                DialogueManager.Instance.StartDialogue(dialogueSO);
+        }
     }
 }
