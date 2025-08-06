ada31e1f9f9e22e1679a94655cd8195a063d3c5d
MinTech <ngonguyenkminh.dsn@gmail.com>
2025-08-06 19:22:48 +0700

    Ađ Main Menu Scene

---
+++ Assets/Scripts/ButtonHoverEffect.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ButtonHoverEffect : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    private Button button;
+    private Color originalColor;
+    public Color hoverColor = Color.yellow;
+
+    void Start()
+    {
+        button = GetComponent<Button>();
+        originalColor = button.image.color;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        button.image.color = hoverColor;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        button.image.color = originalColor;
+    }
+}
---
+++ Assets/Scripts/SelectGameManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class SelectGameManager : MonoBehaviour
+{
+    public void OptionsGame()
+    {
+        //Optional
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
