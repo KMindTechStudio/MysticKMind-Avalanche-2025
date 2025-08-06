ee28e3ccd894ddb9d5db5afec8a89d00c44a83fe
MinTech <ngonguyenkminh.dsn@gmail.com>
2025-08-06 22:22:27 +0700

    Update FadeToWhite when select map in main menu

--- Assets/Scripts/SelectGameManager.cs
+++ Assets/Scripts/SelectGameManager.cs
@@ -1,14 +1,36 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SelectGameManager : MonoBehaviour
 {
+    public Animator fadeAnim;
+    public float fadeTime = .5f;
+
+    private IEnumerator LoadSceneWithFade(string sceneName)
+    {
+        fadeAnim.Play("FadeToWhite");
+        yield return new WaitForSeconds(fadeTime);
+        SceneManager.LoadScene(sceneName);
+    }
+
     public void OptionsGame()
     {
-        //Optional
+        // Optional
     }
 
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    public void LoadMap1()
+    {
+        StartCoroutine(LoadSceneWithFade("Map 1"));
+    }
+
+    public void LoadMap2()
+    {
+        StartCoroutine(LoadSceneWithFade("Map 2"));
     }
 }
